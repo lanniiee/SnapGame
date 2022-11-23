b66b96c4370be5137376c28d9c98aff5ab9ce4fa
lanniiee <lav250397@hotmail.com>
2022-11-23 14:48:02 +0000

    created template for cardGame

---
+++ src/main/java/com/nology/game/cardgame/Card.java
@@ -0,0 +1,16 @@
+package com.nology.game.cardgame;
+
+public class Card {
+
+    private String suit;
+    private String symbol;
+    private int value;
+
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+
+}
---
+++ src/main/java/com/nology/game/cardgame/CardGame.java
@@ -0,0 +1,9 @@
+package com.nology.game.cardgame;
+
+import java.util.ArrayList;
+
+public class CardGame {
+
+    private ArrayList<Card> deckOfCards = new ArrayList<>();
+
+}

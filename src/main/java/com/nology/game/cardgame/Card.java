13a08cf20b6709ec01b2f89c1fc76ff8882afc48
lanniiee <lav250397@hotmail.com>
2022-11-23 17:18:49 +0000

    created 52 cards

--- src/main/java/com/nology/game/cardgame/Card.java
+++ src/main/java/com/nology/game/cardgame/Card.java
@@ -2,15 +2,25 @@ package com.nology.game.cardgame;
 
 public class Card {
 
-    private String suit;
+    // Has a String suit. Use the unicode characters of {heart, club, diamond, and spade}
+    private char suit;
+
+    // Has a string symbol {2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A }
     private String symbol;
+
+    // Has an int value {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
     private int value;
 
-    public Card(String suit, String symbol, int value) {
+
+    public Card(char suit, String symbol, int value) {
         this.suit = suit;
         this.symbol = symbol;
         this.value = value;
     }
 
-
+    // Has a toString method that describe the class
+    @Override
+    public String toString() {
+        return this.symbol + " " + this.suit;
+    }
 }
--- src/main/java/com/nology/game/cardgame/CardGame.java
+++ src/main/java/com/nology/game/cardgame/CardGame.java
@@ -4,6 +4,33 @@ import java.util.ArrayList;
 
 public class CardGame {
 
+    private final char[] suit = new char[] {'\u2764', '\u2660', '\u2663', '\u2666'};
+    private final String[] symbol = new String[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q" , "K", "A"};
+
+    // Contains an ArrayList<Card> for the deckOfCards that contains all 52 cards. This is created and populated when the game is constructed.
     private ArrayList<Card> deckOfCards = new ArrayList<>();
 
+    // Has a name which is also defined in the constructor
+    public CardGame() {
+
+        for (int a = 0; a < suit.length; a++){
+            for (int b = 0; b < symbol.length; b++) {
+                Card card = new Card(suit[a], symbol[b], b + 2);
+                deckOfCards.add(card);
+            }
+        }
+    }
+
+    // Has a getDeck method that lists out the cards in the deck
+
+
+    public ArrayList<Card> getDeckOfCards() {
+        return this.deckOfCards;
+    }
+
+    public static void main(String[] args) {
+
+        CardGame cards = new CardGame();
+        System.out.println(cards.getDeckOfCards());
+    }
 }

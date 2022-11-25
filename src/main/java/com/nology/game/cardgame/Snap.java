3abb8ead89801d263de1a5460846cfec63e33e2b
lanniiee <lav250397@hotmail.com>
2022-11-25 13:08:13 +0000

    cleaning up code

--- src/main/java/com/nology/game/cardgame/Snap.java
+++ src/main/java/com/nology/game/cardgame/Snap.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 
 public class Snap extends CardGame {
 
-    private Scanner scanner = new Scanner(System.in);
-    private ArrayList<Card> deckOfCards;
-    private Card currentCard;
-    private ArrayList<Card> discardPile = new ArrayList<>();
-    private String input;
-    private boolean playingGame = false;
+    private final Scanner scanner = new Scanner(System.in);
+    private final ArrayList<Card> deckOfCards;
+    private final ArrayList<Card> discardPile;
 
     public Snap() {
         deckOfCards = getDeckOfCards();
@@ -21,22 +18,23 @@ public class Snap extends CardGame {
 
     public void runGame() {
         System.out.println("Press Enter to start!");
-        playingGame = true;
+        boolean playingGame = true;
         while (playingGame) {
-            input = scanner.nextLine();
-            currentCard = dealCard();
-            System.out.println(currentCard);
-            if (discardPile.size() > 1) {
-                if (currentCard.getSymbol().equals(discardPile.get(discardPile.size()-2).getSymbol())) {
-                    System.out.println("Well Done! You have won!");
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                Card currentCard = dealCard();
+                System.out.println("Current card: " + currentCard);
+                if (discardPile.size() > 1) {
+                    if (currentCard.getSymbol().equals(discardPile.get(discardPile.size()-2).getSymbol())) {
+                        System.out.println("\nWell Done! You have won!");
+                        playingGame = false;
+                    }
+                }
+                if (deckOfCards.size() == 0) {
+                    System.out.println("\nYou did not win this time!");
                     playingGame = false;
                 }
             }
-            if (deckOfCards.size() == 0) {
-                System.out.println("You did not win this time!");
-                playingGame = false;
-            }
-
         }
     }
 
@@ -44,16 +42,6 @@ public class Snap extends CardGame {
         Snap snap = new Snap();
         snap.runGame();
     }
-
-    // By Pressing enter in the command line, the user takes their turn
-
-
-    // Each turn, a new card is dealt from the deck
-
-
-    // The game continues until two cards in a row have the same symbol, at which point the 'player' wins and the game ends.
-
-
 
 
 }
